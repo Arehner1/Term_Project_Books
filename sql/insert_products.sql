317d06a9f62ef92b7913028f991460d342f16627
austinrehner <arrehner@uncg.edu>
2025-05-06 14:46:49 -0400

    fixed static index.html

--- sql/insert_products.sql
+++ sql/insert_products.sql
@@ -1,5 +1,5 @@
 INSERT INTO products (name, description, image_url, price, status, created_at, category_id) VALUES
-('Mystery Novel', 'An intriguing mystery to keep you guessing.', 'images/Mystery.jpg', 1999, 'featured', '2025-04-01', 3),
-('Sci-Fi Adventure', 'Explore thrilling sci-fi landscapes.', 'images/Sci-fi.jpg', 2499, 'featured', '2025-04-01', 4),
-('Historical Fiction', 'A moving story from the past.', 'images/Historical fiction.jpg', 1499, 'available', '2025-04-01', 6),
-('Fantasy Saga', 'Epic battles and magic await.', 'images/fantasy saga.jpg', 2999, 'featured', '2025-04-01', 5);
+('A Silent Patient', 'An intriguing mystery to keep you guessing.', 'images/Mystery.jpg', 1999, 'featured', '2025-04-01', 3),
+('Halo', 'Explore thrilling sci-fi landscapes.', 'images/Sci-fi.jpg', 2499, 'featured', '2025-04-01', 4),
+('All the Light We Cannot See', 'A moving story from the past.', 'images/Historical fiction.jpg', 1499, 'available', '2025-04-01', 6),
+('A Game of Thrones', 'Epic battles and magic await.', 'images/fantasy saga.jpg', 2999, 'featured', '2025-04-01', 5);
